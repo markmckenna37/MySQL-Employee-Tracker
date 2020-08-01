061c0d2a2c7d8db216cdcaeee67805246407a9af
markmckenna37 <markmckenna37@gmail.com>
2020-07-31 21:04:13 -0600

    added readme filr and comments to js file

--- README.md
+++ README.md
@@ -1,1 +1,64 @@
-# MySQL-Employee-Tracker
+
+# Employee Tracker
+  
+![](/Assets/screenshot.png)
+
+## Testing
+  
+![GitHub License](https://img.shields.io/badge/license-MIT-blue.svg)
+  
+---
+  
+## Description 
+Create and render an employee database for your company! Create different company departments, roles, and employee positions and organize them into convenient tables!
+                      
+--- 
+                      
+## Table of Contents
+                      
+                      
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [License](#license)
+* [Questions](#questions)
+                      
+---
+                      
+## Installation
+                      
+Simply visit the [GitHub repository](https://github.com/markmckenna37/MySQL-Employee-Tracker) and copy the link under the "code" button. Then, with your GitBash or Terminal, type "git clone" and paste the link into the field. Then, type "npm init" and "npm install" to get all of the required files.
+                      
+                      
+---
+                      
+## Usage 
+                      
+After installing, simply type "node index.js", and respond to the proceding prompts!
+                      
+---
+                      
+## Contributing
+                      
+Mark McKenna
+                      
+---
+                      
+## License
+                      
+This project is licensed under MIT.
+  
+Copyright (c) [2020] [Mark McKenna]
+  
+                      
+---
+                                                         
+## Questions
+Have any questions or contributions? Check out my [GitHub Profile](https://github.com/markmckenna37)                 
+Or you can email me at <markmckenna37@gmail.com>.
+                      
+---
+                      
+## Tests
+                      
+
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//requring sql, inquirer, console.table
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const table = require("console.table")
@@ -11,7 +12,7 @@ const connection = mysql.createConnection({
 });
 
 
-
+//function to add departments. returns an inquier prompt, then inserts the user's input into the departments table
 addDepartment = () => {
     return inquirer.prompt([{
         name: "name",
@@ -21,9 +22,9 @@ addDepartment = () => {
         name
     }) => {
         connection.query("INSERT INTO departments SET ?", {
-                id: id,
-                name: name
-            },
+            id: id,
+            name: name
+        },
             (err, result) => {
                 if (err) throw err;
                 console.log(`Added ${name} to company departments`);
@@ -32,11 +33,12 @@ addDepartment = () => {
         )
     })
 }
-
+//function to get a specific department ID, recieves parameters from store roles.
 function getDepartmentId(department) {
     return new Promise((resolve, reject) => {
         let id;
         const arg = [department]
+        //Query to get data from departments table
         connection.query(`SELECT id FROM departments WHERE departments.name = '${arg}'`, (err, data) => {
             if (err) reject(err)
             id = data.id;
@@ -45,10 +47,11 @@ function getDepartmentId(department) {
     })
 
 }
-
+//function to get department names for add role function
 function getDepartmentNames() {
     return new Promise((resolve, reject) => {
         let departments = [];
+        //query to get data from departments table
         connection.query("SELECT name FROM departments", (err, data) => {
             if (err) reject(err);
             for (const department of data) {
@@ -59,76 +62,51 @@ function getDepartmentNames() {
 
     })
 }
-async function getDepartmentTable() {
-    const Departments = await getDepartmentNames();
-    console.log(table)
-    let tableArr = []
-    let idArr = []
-    for (let i = 0; i < table.length; i++) {
-        tableArr.push(table[i].name)
-        idArr.push(i)
-    }
-    console.table([Departments])
-    userPrompt();
-}
-
-async function getEmployeeTable() {
-    const query = `SELECT employees.id AS 'ID',
-    first_name AS 'First Name',
-    last_name AS 'Last Name',
-    roles.title AS 'Title',
-    departments.name AS 'Department',
-    roles.salary AS 'Salary',
-    manager_id AS 'Manager ID'
-    FROM employees, roles, departments
-    WHERE employees.role_id = roles.id
-    AND roles.department_id = departments.id
-    ORDER BY employees.id ASC`;
-    const table = await connection.query(query, (err, data) => {
-        if (err) throw (err);
-        console.table(data)
-    })
-}
+//Function to add new role. Awaits department names and renders them in the inquirer prompt.
 async function addRoles() {
     const departments = await getDepartmentNames()
     return inquirer.prompt([{
-            name: "title",
-            message: "What is the title of the role you want to add?"
+        name: "title",
+        message: "What is the title of the role you want to add?"
 
-        },
-        {
-            name: "salary",
-            message: "What is the annual salary of the role you want to add?"
-        },
-        {
-            type: "list",
-            name: "department",
-            message: "What department do you want to add your role to?",
-            choices: [...departments]
-        }
+    },
+    {
+        name: "salary",
+        message: "What is the annual salary of the role you want to add?"
+    },
+    {
+        type: "list",
+        name: "department",
+        message: "What department do you want to add your role to?",
+        choices: [...departments]
+    }
     ]).then(({
         title,
         salary,
         department
     }) => {
+        //calling function, passing data in as parameters.
         storeRoles(title, salary, department)
     })
 }
-
-async function storeRoles(title, salary, department){
-        const departmentId = await getDepartmentId(department);
-        const arg1 = title;
-        const arg2 = salary;
-        await connection.query(`INSERT INTO roles (title, salary, department_id)
+//Function to store roles to the database table, recieves data from addroles
+async function storeRoles(title, salary, department) {
+    //awaiting department ID
+    const departmentId = await getDepartmentId(department);
+    const arg1 = title;
+    const arg2 = salary;
+    //Query to insert data into roles table
+    await connection.query(`INSERT INTO roles (title, salary, department_id)
          VALUES ('${arg1}', ${arg2}, ${departmentId[0].id})`, (err, data) => {
-            if (err) throw err;
-            console.log(`Successfully added ${arg1} to company roles.`)
-            userPrompt()
-        });
+        if (err) throw err;
+        console.log(`Successfully added ${arg1} to company roles.`)
+        userPrompt()
+    });
 
 }
-
+//Function to get role id
 function getRoleId(role) {
+    //Returns a promise for the querty to pull data from the roles table
     return new Promise((resolve, reject) => {
         connection.query(`SELECT id FROM roles WHERE roles.title = '${role}'`, (err, data) => {
             if (err) reject(err);
@@ -137,7 +115,9 @@ function getRoleId(role) {
 
     })
 }
+//Function to get the titles of the roles
 function getRoleNames() {
+        //Returns a promise for the querty to pull data from the roles table
     return new Promise((resolve, reject) => {
         let roles = [];
         connection.query("SELECT title FROM roles", (err, data) => {
@@ -150,10 +130,57 @@ function getRoleNames() {
 
     })
 }
-async function getRoleTable() {
-    
+//Function to console.table the departments table
+function getDepartmentTable() {
+    const query = `SELECT departments.id AS 'ID',
+    departments.name AS 'Department Name'
+    FROM departments
+    ORDER BY departments.id ASC`;
+    const table = connection.query(query, (err, data) => {
+        if (err) throw err;
+        console.log('\n')
+        console.table(data);
+    })
+    userPrompt();
+}
+//Function to console.table the roles table
+function getRoleTable() {
+    const query = `SELECT roles.id AS 'ID',
+    title AS 'Title',
+    salary AS 'Salary',
+    roles.department_id AS 'Department ID'
+    FROM departments, roles
+    WHERE roles.department_id = departments.id
+    ORDER BY roles.id ASC`;
+    const table = connection.query(query, (err, data) => {
+        if (err) throw (err);
+        console.log('\n')
+        console.table(data)
+    })
+    userPrompt();
 }
 
+//Function to console.table the employees table
+function getEmployeeTable() {
+    const query = `SELECT employees.id AS 'ID',
+    first_name AS 'First Name',
+    last_name AS 'Last Name',
+    roles.title AS 'Title',
+    departments.name AS 'Department',
+    roles.salary AS 'Salary',
+    manager_id AS 'Manager ID'
+    FROM employees, roles, departments
+    WHERE employees.role_id = roles.id
+    AND roles.department_id = departments.id
+    ORDER BY employees.id ASC`;
+    const table = connection.query(query, (err, data) => {
+        if (err) throw (err);
+        console.log('\n')
+        console.table(data)
+    })
+    userPrompt();
+}
+//Inquirer prompt to add employees, waits for role names, and employee names to pass them as choices.
 async function addEmployees() {
     const roles = await getRoleNames()
     const names = await getEmployeeNames();
@@ -186,15 +213,18 @@ async function addEmployees() {
         role,
         manager
     }) => {
+        //calling function to store employees to the database
         storeEmployee(firstName, lastName, role, manager);
     })
 }
-async function storeEmployee(firstName, lastName, role, manager){
+//Function to store employees to the database, recieves parameters from add employees function
+async function storeEmployee(firstName, lastName, role, manager) {
     const arg1 = firstName;
     const arg2 = lastName;
+    //Awaiting role id and manager id
     const arg3 = await getRoleId(role);
     const arg4 = await getManagerId(manager)
-    console.log(arg4);
+    //Query to insert data into employees table
     await connection.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id)
      VALUES ('${arg1}', '${arg2}', ${arg3[0].id}, ${arg4[0].id})`, (err, data) => {
         if (err) throw err;
@@ -203,7 +233,7 @@ async function storeEmployee(firstName, lastName, role, manager){
     });
 
 }
-
+//Function to get employee names to use in inquirer prompt
 function getEmployeeNames() {
     return new Promise((resolve, reject) => {
         let names = [];
@@ -217,9 +247,9 @@ function getEmployeeNames() {
 
     })
 }
-
+//Function to get manager ID to use in inquirer prompt
 function getManagerId(manager) {
-   const managerId = manager.split(" ")
+    const managerId = manager.split(" ")
     return new Promise((resolve, reject) => {
         connection.query(`SELECT id FROM employees WHERE employees.last_name = '${managerId[1]}'`, (err, data) => {
             if (err) reject(err);
@@ -228,7 +258,7 @@ function getManagerId(manager) {
 
     })
 }
-
+//Initial prompt for the user to choose their desired action.
 function userPrompt() {
     return inquirer.prompt({
         type: "list",
@@ -247,6 +277,7 @@ function userPrompt() {
     }).then(({
         choices
     }) => {
+        //Feeding choices into a switch case. Callback functions for each case
         switch (choices) {
             case "Add departments":
                 addDepartment();
@@ -258,10 +289,10 @@ function userPrompt() {
                 addEmployees();
                 break;
             case "View departments":
-              getDepartmentTable()
+                getDepartmentTable();
                 break;
             case "View roles":
-                getRoleNames();
+                getRoleTable();
                 break;
             case "View employees":
                 getEmployeeTable();
@@ -270,15 +301,17 @@ function userPrompt() {
                 console.log("7")
                 break;
             default:
-                console.log("K BYE");
+                console.log("Thanks! See you soon!");
+                connection.end();
         }
     })
 };
 
 
-
+//Connection 
 connection.connect((err) => {
     if (err) throw (err);
-    console.log("Success!")
+    console.log("Successfully connected as id " + connection.threadId)
+    console.log("Welcome to the Employee Database Tracker!");
     userPrompt()
 });
